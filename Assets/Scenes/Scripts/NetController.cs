e90faeb06554adab2ff3155522a5b5577fdeba73
Даниил Азаров <azarov.daniil@inbox.ru>
2024-06-07 16:10:31 +0300

    some

--- Assets/Scenes/Scripts/NetController.cs
+++ Assets/Scenes/Scripts/NetController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -8,6 +9,20 @@ public class NetController : NetworkBehaviour
     public float moveSpeed = 5f;
 
     private NetworkVariable<Vector3> position = new NetworkVariable<Vector3>();
+
+    public void Start()
+    {
+        NetworkManager.Singleton.StartHost();
+        var v = Dns.GetHostEntry(Dns.GetHostName());
+        foreach(var v2 in v.AddressList)
+        {
+            Debug.Log(v2.ToString());
+            if (v2.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                Debug.Log("Internet host " + v2.ToString());
+            }
+        }
+    }
 
     private void Update()
     {
